ad09244416e37527d87dbf556a757da751f7028e
BillyBajorek <williambajorek@gmail.com>
2024-09-29 01:58:19 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const cacheName = 'note-taking-app-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/manifest.json',
+  '/images/icon-192x192.png',
+  '/images/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then((cache) => {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
